1e4d660d5910dd1736f205328cb86233803a3052
cedric de sereys <desereyscedric@gmail.com>
2023-06-24 13:34:48 +0200

    fine tuning div

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_20_093043) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_24_082348) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_20_093043) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "status", default: 0
+    t.text "message"
     t.index ["flat_id"], name: "index_rentals_on_flat_id"
     t.index ["user_id"], name: "index_rentals_on_user_id"
   end
