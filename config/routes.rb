3eadcb31aed712ae8afb5ef0ae36c92b00b72756
Anais Segall <anais.segall@gmail.com>
2023-06-25 22:25:24 +0200

    Adding profile pic

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "pages#home"
   resources :flats
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
