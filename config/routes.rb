0df52d835d9ec0a0f02a2105f5b9f15c6120d82f
Anais Segall <anais.segall@gmail.com>
2023-06-26 10:03:38 +0200

    merge

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
   resources :flats
-
-  devise_for :users, controllers: {
-    registrations: 'users/registrations'
-  }
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'your_spaces/new'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_20_093043) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_23_191343) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -99,9 +99,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_20_093043) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-
   create_table "your_spaces", force: :cascade do |t|
     t.boolean "tv"
     t.boolean "bathroom"
@@ -114,6 +111,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_20_093043) do
     t.index ["flat_id"], name: "index_your_spaces_on_flat_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "flats", "users"
   add_foreign_key "quizzs", "users"
   add_foreign_key "rentals", "flats"
