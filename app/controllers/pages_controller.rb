b8b0744b47bdc6fff2f5f6ff8f0e8e519446dd0d
guillaume lhostis <guillaume.lhostis@free.fr>
2023-06-29 18:23:16 +0200

    front and algo and candidat

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,21 +11,16 @@ class PagesController < ApplicationController
 
   def accept_rental
     @rental = Rental.find(params[:format].to_i)
-    @rental.status == "accepted"
-    redirect_to pages_dashboard_path
+    @rental.status = "accepted"
+    @rental.save
+    redirect_to pages_senior_dashboard_path
   end
 
   def refuse_rental
     @rental = Rental.find(params[:format].to_i)
-    @rental.status == "refused"
-    redirect_to pages_dashboard_path
-  end
-
-  def compatibily(results_a, results_b)
-    a = [results_a[:smoker], results_a[:talker], results_a[:guest], results_a[:motivation], results_a[:gender], results_a[:animal], results_a[:guest_animal], results_a[:junior_smoker], results_a[:music]]
-    b = [results_b[:smoker], results_b[:talker], results_b[:guest], results_b[:motivation], results_b[:gender], results_b[:animal], results_b[:guest_animal], results_b[:junior_smoker], results_b[:music]]
-
-    matching = matching_algo(a, b)
+    @rental.status = "refused"
+    @rental.save
+    redirect_to pages_senior_dashboard_path
   end
 
   def senior_dashboard
@@ -38,6 +33,14 @@ class PagesController < ApplicationController
       @rentals << rentals_requests
     else
       @rentals = rentals_requests
+    end
+    @matching = []
+    @juniors_quizz = []
+    if rentals_requests.present?
+      @rentals.each_with_index do |rental, index|
+        @juniors_quizz << Quizz.find_by(user_id: rental.user_id)
+        @matching << { id: rental.id, match: compatibily(@quizz, @juniors_quizz[index]), user_id: rental.user_id }
+      end
     end
   end
 
@@ -53,7 +56,7 @@ class PagesController < ApplicationController
       end
       @matching = []
       @flats.each_with_index do |flat, index|
-        @matching << {id: flat.id, match: compatibily(@quizz, @seniors_quizz[index]), user_id: flat.user_id}
+        @matching << { id: flat.id, match: compatibily(@quizz, @seniors_quizz[index]), user_id: flat.user_id }
       end
 
       @markers = @flats.map do |flat|
@@ -74,6 +77,13 @@ class PagesController < ApplicationController
     @quizz = Quizz.find_by(user_id: current_user.id)
   end
 
+  def candidat
+    @rental = Rental.find(params[:format])
+    @user = current_user
+    @junior = User.find(@rental.user_id)
+    @matching = compatibily(Quizz.find_by(user_id: @junior.id), Quizz.find_by(user_id: current_user.id))
+  end
+
   private
 
   def matching_algo(a, b)
@@ -81,5 +91,12 @@ class PagesController < ApplicationController
     percent = result * 100
     final_result = 100 - percent
     return "#{final_result.round(2)} %"
+  end
+
+  def compatibily(results_a, results_b)
+    a = [results_a[:smoker], results_a[:talker], results_a[:guest], results_a[:motivation], results_a[:gender], results_a[:animal], results_a[:guest_animal], results_a[:junior_smoker], results_a[:music]]
+    b = [results_b[:smoker], results_b[:talker], results_b[:guest], results_b[:motivation], results_b[:gender], results_b[:animal], results_b[:guest_animal], results_b[:junior_smoker], results_b[:music]]
+
+    matching = matching_algo(a, b)
   end
 end
