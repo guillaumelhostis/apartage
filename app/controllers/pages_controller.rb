957a18db36015cce62883d02abca688ddf45745a
Anais Segall <anais.segall@gmail.com>
2023-07-02 18:55:13 +0200

    Reprise du setup clean

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -95,6 +95,8 @@ class PagesController < ApplicationController
     @rental = Rental.find(params[:format])
     @user = current_user
     @junior = User.find(@rental.user_id)
+    @quizz = Quizz.find_by(user_id: @junior.id)
+    @quizz_file = @quizz.file if @quizz.present?
     @matching = compatibily(Quizz.find_by(user_id: @junior.id), Quizz.find_by(user_id: current_user.id))
   end
 
@@ -115,6 +117,11 @@ class PagesController < ApplicationController
   end
 
   def compatibily(results_a, results_b)
+    if results_a.nil? || results_b.nil?
+      # Gérer le cas où les résultats ne sont pas définis ou sont vides
+      return nil
+    end
+
     a = [results_a[:smoker], results_a[:talker], results_a[:guest], results_a[:motivation], results_a[:gender], results_a[:animal], results_a[:guest_animal], results_a[:junior_smoker], results_a[:music]]
     b = [results_b[:smoker], results_b[:talker], results_b[:guest], results_b[:motivation], results_b[:gender], results_b[:animal], results_b[:guest_animal], results_b[:junior_smoker], results_b[:music]]
 
